7c0a1bf391236f1b8ed7eb4ade6d9e053edc66c3
ucctn <chetan.singhal@ucertify.com>
2026-08-02 11:05:43 -0600

    stopping point, working on dataentry

--- courseProject/src/courseProject/DataEntry.java
+++ courseProject/src/courseProject/DataEntry.java
@@ -1,5 +1,48 @@
 package courseProject;
 
-public class DataEntry {
+import java.util.Scanner;
 
+public class DataEntry {
+	Scanner scanner = new Scanner(System.in);
+	
+	public String getString(String prompt) {
+		System.out.print(prompt);
+		return scanner.nextLine();
+	}
+	
+	public String getString(String prompt, int max) {
+		String input;
+		while (true) {
+			System.out.print(prompt);
+			input = scanner.nextLine();
+			if (input.length() <= max) {
+				return input;
+			}
+			System.out.println("Error: Input exceeds maximum length of " + max +" characters");
+		}
+	}
+	
+	public String getNumString(String prompt) {
+		while (true) {
+			System.out.print(prompt);
+			String input = scanner.nextLine();
+			if (input.matches("\\d+")) {
+				return input;
+			}
+			System.out.println("Error: Input must contain numbers only.");
+		}	
+	}
+	
+	public int getInt(String prompt) {
+		while (true) {
+			System.out.print(prompt);
+			if (scanner.hasNextInt()) {
+				int value = scanner.nextInt();
+				scanner.nextLine();
+				return value;
+				}
+			System.out.println("Error: Please enter an integer");
+			scanner.nextLine();
+		}
+	}
 }
